344721a5c76e73928460175ec4c73a76cc724f13
keshav <kdooleea@yahoo.ca>
2020-12-02 20:17:23 -0500

    added output emitter for navbar

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  showNavbar: boolean;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onNavbarChanged(showNav: boolean): void {
+    this.showNavbar = showNav;
   }
 
+  showNav(): void {
+    this.showNavbar = true;
+  }
+
+  hideNav(): void {
+    this.showNavbar = false;
+  }
 }
--- frontend/src/app/components/home/navbar/navbar.component.ts
+++ frontend/src/app/components/home/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,11 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  @Output() showNavbarEmitter = new EventEmitter<boolean>();
+
   constructor() {}
 
   ngOnInit(): void {}
 
   onLogOutClicked(): void {
     console.log('Logged out clicked');
+  }
+
+  showNavbar(): void {
+    this.showNavbarEmitter.emit(true);
+  }
+
+  hideNavbar(): void {
+    this.showNavbarEmitter.emit(false);
   }
 }
