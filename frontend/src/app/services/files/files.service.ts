763e9bd310d03c1b50cf20efcbfdacfea52b81bc
keshav <kdooleea@yahoo.ca>
2020-12-05 12:08:20 -0500

    separated file type detection into own service

--- frontend/src/app/services/files/files.service.ts
+++ frontend/src/app/services/files/files.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { CustomFiles } from 'src/app/classes/files';
+import { InnerHtmlService } from 'src/app/services/files/inner-html/inner-html.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ export class FilesService {
   private files: CustomFiles[];
   private fileSubscription: BehaviorSubject<CustomFiles[]>;
 
-  constructor() {
+  constructor(private innerHtmlService: InnerHtmlService) {
     this.files = [];
     this.fileSubscription = new BehaviorSubject<CustomFiles[]>(this.files);
   }
@@ -24,11 +25,14 @@ export class FilesService {
       this.getBase64(newFiles[i])
         .then((imageData) => {
           this.files.push({
+            isLocked: false,
             base64: imageData,
             name: newFiles[i].name,
             date: newFiles[i].lastModified,
-            innerHtml: this.addInnerHTML(imageData, newFiles[i].name),
-            isLocked: false,
+            innerHtml: this.innerHtmlService.getInnerHTML(
+              imageData,
+              newFiles[i].name
+            ),
           });
         })
         .catch((error) => console.log(error));
@@ -63,79 +67,5 @@ export class FilesService {
       reader.onload = () => resolve(reader.result);
       reader.onerror = (error) => reject(error);
     });
-  }
-
-  private addInnerHTML(
-    imageData: string | ArrayBuffer,
-    fileName: string
-  ): string {
-    // images
-    if (
-      fileName.endsWith('jpeg') ||
-      fileName.endsWith('png')
-      // fileName.endsWith('svg')
-    ) {
-      return `<img src="${imageData}" alt="imageBase64" class="img-html"/>`;
-    }
-
-    // videos
-    if (fileName.endsWith('mp4')) {
-      return '<i class="fas fa-file-video icon"></i>';
-    }
-
-    // icon
-    if ((imageData as string).startsWith('data:image/x-icon;base64,')) {
-      return '<i class="fas fa-file-image icon"></i>';
-    }
-
-    // pdf
-    if (fileName.endsWith('pdf')) {
-      return `<i class="fas fa-file-pdf icon"></i>`;
-    }
-
-    // txt files
-    if (fileName.endsWith('txt') || fileName.endsWith('md')) {
-      return `<i class="fas fa-file-alt icon"></i>`;
-    }
-
-    // word doc
-    if (fileName.endsWith('doc') || fileName.endsWith('docx')) {
-      return `<i class="fas fa-file-word icon"></i>`;
-    }
-
-    // excel files
-    if (fileName.endsWith('xlsx')) {
-      return `<i class="fas fa-file-excel icon"></i>`;
-    }
-
-    // powerpoint
-    if (fileName.endsWith('ppt')) {
-      return `<i class="fas fa-file-powerpoint icon"></i>`;
-    }
-
-    // audio
-    if (fileName.endsWith('mp3')) {
-      return `<i class="fas fa-file-audio icon"></i>`;
-    }
-
-    // zip files
-    if (fileName.endsWith('zip') || fileName.endsWith('rar')) {
-      return `<i class="fas fa-file-archive icon"></i>`;
-    }
-
-    // code files
-    if (
-      fileName.endsWith('ts') ||
-      fileName.endsWith('js') ||
-      fileName.endsWith('css') ||
-      fileName.endsWith('scss') ||
-      fileName.endsWith('c++') ||
-      fileName.endsWith('html')
-    ) {
-      return `<i class="fas fa-file-code icon"></i>`;
-    }
-
-    // other types
-    return `<i class="fas fa-file icon other-icon"></i>`;
   }
 }
