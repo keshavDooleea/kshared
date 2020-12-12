6308b7a854af9f33bfd1fdeaed7b3f03da34b176
keshav <kdooleea@yahoo.ca>
2020-12-11 22:47:16 -0500

    completed register UI form

--- frontend/src/app/classes/validator.ts
+++ frontend/src/app/classes/validator.ts
@@ -1,11 +1,8 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
 
 const MIN_LENGTH = 5;
-const MAX_LENGTH = 10;
 
-type ValidatorFn = (control: AbstractControl) => ValidationErrors | null;
-
-export const validateUsername = (
+export const validateLength = (
   control: AbstractControl
 ): { [key: string]: boolean } | null => {
   if (control.value.length >= 0 && control.value.length < MIN_LENGTH) {
@@ -13,22 +10,4 @@ export const validateUsername = (
   }
 
   return null;
-};
-
-export const validatePassword = (
-  control: AbstractControl
-): { [key: string]: boolean } | null => {
-  if (control.value.length < MIN_LENGTH) {
-    return { shortError: true };
-  }
-
-  return null;
-};
-
-export const confirmPassword = (p1: string, p2: string): ValidatorFn => {
-  return (control: AbstractControl): ValidationErrors | null => {
-    // Validaton code here
-    console.log(p1, p2);
-    return null;
-  };
 };
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { validateLength } from 'src/app/classes/validator';
 
 @Component({
   selector: 'app-login',
@@ -8,19 +10,54 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   isRegister: boolean;
   shouldShowModal: boolean;
+  registerForm: FormGroup;
 
   constructor() {
     this.isRegister = false;
     this.shouldShowModal = false;
+
+    this.initialiseRegisterForm();
+  }
+
+  initialiseRegisterForm(): void {
+    this.registerForm = new FormGroup({
+      username: new FormControl(
+        '',
+        Validators.compose([Validators.required, validateLength])
+      ),
+      password: new FormControl(
+        '',
+        Validators.compose([Validators.required, validateLength])
+      ),
+      confirmPassword: new FormControl(
+        '',
+        Validators.compose([Validators.required])
+      ),
+    });
   }
 
   ngOnInit(): void {}
 
   onCancelClicked(): void {
     this.isRegister = false;
+    this.initialiseRegisterForm();
   }
 
   onRegisterClicked(): void {
+    if (
+      this.registerForm.invalid ||
+      this.registerPassword !== this.registerConfirmPassword
+    ) {
+      return;
+    }
+
+    const registerForm = {
+      username: this.registerUsername,
+      password: this.registerPassword,
+    };
+
+    console.log(registerForm);
+
     // this.isRegister = false;
   }
 
@@ -32,5 +69,17 @@ export class LoginComponent implements OnInit {
 
   toggleModal(): void {
     this.shouldShowModal = !this.shouldShowModal;
+  }
+
+  get registerUsername(): string {
+    return this.registerForm.get('username').value;
+  }
+
+  get registerPassword(): string {
+    return this.registerForm.get('password').value;
+  }
+
+  get registerConfirmPassword(): string {
+    return this.registerForm.get('confirmPassword').value;
   }
 }
