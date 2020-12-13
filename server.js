9882214bbd63e7b118812b9097f8f7c091a5112a
keshav <kdooleea@yahoo.ca>
2020-12-13 14:33:48 -0500

    refactored functions

--- server.js
+++ server.js
@@ -48,61 +48,77 @@ io.on("connection", (socket) => {
   });
 
   socket.on("deleteAccount", async (token) => {
-    const user = findUser(token);
-
-    try {
-      await User.findByIdAndDelete(user.id);
-    } catch (err) {
-      console.log("Delete account error: ", err);
-    }
+    await deleteAccount(token);
   });
 
   // when browser refreshes, get user details
   socket.on("pageRefresh", async (data) => {
-    const user = findUser(data);
-
-    try {
-      let currentUser = await User.findById(user.id);
-
-      // update user details
-      user.currentText = currentUser.currentText;
-      user.noteList = currentUser.notes;
-
-      socket.emit("initialLanding", user);
-    } catch (error) {
-      console.log("Updating current text error: ", error);
-    }
+    await pageRefresh(data, socket);
   });
 
   socket.on("updateText", async (data) => {
-    try {
-      currentTypingText = data.text;
-
-      io.emit("updatedText", currentTypingText);
-    } catch (error) {
-      console.log("Updating current text error: ", error);
-    }
+    await updateText(data, io);
   });
 
   // new note being saved
   socket.on("saveNoteList", async (data) => {
-    const user = findUser(data.token);
-
-    try {
-      let currentUser = await User.findById(user.id);
-      currentUser.notes = data.notes;
-      await currentUser.save();
-
-      io.emit("getNotes", currentUser.notes);
-    } catch (error) {
-      console.log("Updating current note error: ", error);
-    }
+    await saveNoteList(data, io);
   });
 
-  socket.on("disconnect", () => {
-    saveText(token);
+  socket.on("disconnect", async () => {
+    await saveText(token);
   });
 });
+
+const deleteAccount = async (token) => {
+  const user = findUser(token);
+
+  try {
+    await User.findByIdAndDelete(user.id);
+  } catch (err) {
+    console.log("Delete account error: ", err);
+  }
+};
+
+const pageRefresh = async (data, socket) => {
+  const user = findUser(data);
+
+  try {
+    let currentUser = await User.findById(user.id);
+
+    // update user details
+    user.currentText = currentUser.currentText;
+    user.noteList = currentUser.notes;
+
+    socket.emit("initialLanding", user);
+  } catch (error) {
+    console.log("Updating current text error: ", error);
+  }
+};
+
+const updateText = async (data, io) => {
+  try {
+    currentTypingText = data.text;
+
+    io.emit("updatedText", currentTypingText);
+  } catch (error) {
+    console.log("Updating current text error: ", error);
+  }
+};
+
+const saveNoteList = async (data, io) => {
+  const user = findUser(data.token);
+
+  try {
+    let currentUser = await User.findById(user.id);
+    currentUser.notes = data.notes;
+    await currentUser.save();
+
+    io.emit("getNotes", currentUser.notes);
+  } catch (error) {
+    console.log("Updating current note error: ", error);
+  }
+};
 
 const findUser = (data) => {
   let decodedUser = jwt.verify(data, process.env.JWT_TOKEN);
