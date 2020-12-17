728690a46395fd4f66267add8556385574d1c0a3
keshav <kdooleea@yahoo.ca>
2020-12-17 18:40:17 -0500

    changed __dirname

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const io = require("socket.io")(server, {
   },
 });
 
-app.use(express.static(__dirname + "dist/frontend"));
+app.use(express.static(__dirname + "/dist/frontend"));
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "dist/frontend/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/frontend/index.html"));
 });
 
 mongo.connect(
