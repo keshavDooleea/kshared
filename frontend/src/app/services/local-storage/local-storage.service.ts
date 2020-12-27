cdabb77e3e0355ec528c4de19fb4c5ed3490cc1a
keshav <kdooleea@yahoo.ca>
2020-12-27 14:30:02 -0500

    removed old token

--- frontend/src/app/services/local-storage/local-storage.service.ts
+++ frontend/src/app/services/local-storage/local-storage.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../user/user.service';
 
-const STORAGE_KEY = 'kshared-user';
+const STORAGE_KEY = 'kshared-token';
+const OLD_KEY = 'kshared-user';
 
 @Injectable({
   providedIn: 'root',
@@ -16,11 +17,18 @@ export class LocalStorageService {
   }
 
   getToken(): string | null {
+    this.removeOldToken();
     return localStorage.getItem(STORAGE_KEY);
   }
 
   clearToken(): void {
     localStorage.removeItem(STORAGE_KEY);
     this.router.navigateByUrl('/login');
+  }
+
+  private removeOldToken(): void {
+    if (localStorage.getItem(OLD_KEY) !== null) {
+      localStorage.removeItem(OLD_KEY);
+    }
   }
 }
